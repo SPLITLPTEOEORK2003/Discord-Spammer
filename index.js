03e624d95ee04bb50a39be9dc4ff20efe35f69e0
Sellmox5 <42848880+SPLITLPTEOEORK2003@users.noreply.github.com>
2018-10-28 09:55:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const emitter = new EventEmitter();
 
 emitter.setMaxListeners(Number.POSITIVE_INFINITY);
 
-var tokens = [];
+var tokens = ["MzI1MTU3ODA5MjExNTcyMjI1.DOQYbA.vnBfE7DtN2zxZW6Ohw_POto6npA
+MTYzODM3OTQ3OTY2MTI4MTI4.DOQApA.q-RZqUVpg2drGqpSjatcZf0EJZY
+Mjc2MDY1OTc4MzM1NjI1MjE2.DNTGNw.cbNgca_1_9mJ9dal7bdnNkLcPxE"];
 var proxies = [];
 var verified = [];
 var unverified = [];
